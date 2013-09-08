18c0d0f654a2198668b92d958a68b20a4d8eba7c
balika011 <balika011@gmail.com>
2013-09-08 03:36:46 +0200

    Clean up CHook.cpp

--- Client/IVCore/Game/CHooks.cpp
+++ Client/IVCore/Game/CHooks.cpp
@@ -26,26 +26,8 @@ IVPlayerInfo * pReturnedPlayerInfo = NULL;
 unsigned int   uiReturnedIndex = 0;
 IVPlayerPed  * _pPlayerPed = NULL;
 bool           g_bInvalidIndex = false;
-IVPool       * g_pIVPool;
-BYTE         * g_pPoolAllocatedMemory;
-DWORD			dwSub7Jmp;
-DWORD			dwSub7BCall;
-DWORD			dwSub7BJmp;
-char			*szFile;
-bool			bInitialiseGame = true;
-DWORD			dwJumpGame;
-int				iFrameCalls = 0;
 
-int				hFile2;
-HANDLE			iTexture;
-int				v2;// edi@1
-signed int		v3;// esi@1
-int				v4;// ecx@2
-int				v5;// eax@3
-int				result;// eax@5
-DWORD			dwJump;
-
-_declspec(naked) void TextureSelect_Hook()
+/*_declspec(naked) void TextureSelect_Hook()
 {   
 	_asm	mov eax, [esp+4];
 	_asm	mov iTexture, eax;
@@ -62,8 +44,7 @@ _declspec(naked) void TextureSelect_Hook()
 	_asm	push edi;
 	_asm	mov edi, ecx;
 	_asm	jmp COffsets::IV_Hook__TextureSelect__JmpBack;
-}
-
+}*/
 
 _declspec(naked) void CTask__Destructor_Hook()
 {
@@ -81,7 +62,7 @@ _declspec(naked) void CTask__Destructor_Hook()
 	_asm	jmp COffsets::RETURN_CTask__Destructor;
 }
 
-_declspec(naked) void CEpisodes__IsEpisodeAvaliable_Hook()
+/*_declspec(naked) void CEpisodes__IsEpisodeAvaliable_Hook()
 {
 	_asm	mov eax, [esp+4];
 	_asm	test eax, eax;
@@ -91,27 +72,10 @@ _declspec(naked) void CEpisodes__IsEpisodeAvaliable_Hook()
 	_asm	it_not:;
 	_asm	xor al, al;
 	_asm	retn 4;
-}
-
-void StartGame_Loading()
-{
-	int * v7 = new int(0);
-	_asm
-	{
-		push v7
-		mov ecx, COffsets::IV_Hook__StartGameLoading_1
-		call COffsets::IV_Hook__StartGameLoading_2
-	}
-	*(DWORD*)COffsets::IV_Hook__StartGameLoading_3 = *v7;
-	*(DWORD*)COffsets::IV_Hook__StartGameLoading_4 = *v7;
-	if ( *v7 > 0 )
-		_asm call COffsets::IV_Hook__StartGameLoading_5;
-}
+}*/
 
 void RemoveInitialLoadingScreens()
 {
-	StartGame_Loading();
-
 	int iLoadScreens = COffsets::VAR_NumLoadingScreens;
 	int iLoadScreenType = COffsets::VAR_FirstLoadingScreenType;
 	int iLoadScreenDuration = COffsets::VAR_FirstLoadingScreenDuration;
@@ -127,37 +91,9 @@ void RemoveInitialLoadingScreens()
 
 	*(DWORD *)(iLoadScreenDuration + 400) = 5000; // load directx
 	*(DWORD *)(iLoadScreenDuration + 1600) = 5000; // logo screen
-
-	/*
-	(0)0x0 - 5B - [ - 0xE37048
-	(1)0x1 - 31 - 1
-	(2)0x2 - 36 - 6
-	(3)0x3 - 3A - :
-	(4)0x4 - 39 - 9
-	(5)0x5 - 5D - ]
-	(6)0x6 - 00 - placeholder
-	(7)0x7 - 00 - placeholder
-	(8)0x8 - 5B - [
-	(9)0x9 - 31 - 1
-	(10)0xA - 36 - 6
-	(11)0xB - 3A - :
-	(12)0xC - 39 - 9
-	(13)0xD - 5D - ]
-	(14)0xE - 00 - placeholder
-	(15)0xF - 00 - placeholder
-	*/
-
-	/*
-	CPatcher::Unprotect(g_pCore->GetBase() + 0xE37048,15);
-	*(DWORD *)(g_pCore->GetBase() + (0xE37048 + 0x1)) = 0x31363A39; // 16:9
-	*(DWORD *)(g_pCore->GetBase() + (0xE37048 + 0x5)) = 0x5D00005B; // ]*PAD**PAD*[
-	*(DWORD *)(g_pCore->GetBase() + (0xE37048 + 0x9)) = 0x31363A39; // 16:9
-	*(WORD *)(g_pCore->GetBase() + (0xE37048 + 0x13)) = 0x5D00; // ]*PAD*
-	*/
 }
 
-
-IVPlayerInfo * GetPlayerInfoFromIndex(unsigned int uiIndex)
+IVPlayerInfo * __cdecl GetPlayerInfoFromIndex(unsigned int uiIndex)
 {
 	pReturnedPlayerInfo = g_pCore->GetGame()->GetPools()->GetPlayerInfoFromIndex(0);
 
@@ -236,18 +172,6 @@ IVPlayerPed * GetLocalPlayerPed()
 	return _pPlayerPed;
 }
 
-
-IVPlayerPed * GetPlayerPedFromPlayerInfo(IVPlayerInfo * pPlayerInfo)
-{
-	// Is the player info pointer valid?
-	if(pPlayerInfo)
-		_pPlayerPed = pPlayerInfo->m_pPlayerPed;
-	else // Player info pointer is invalid, use the local player ped
-		_pPlayerPed = GetLocalPlayerPed();
-
-	return _pPlayerPed;
-}
-
 _declspec(naked) void GetPlayerInfoFromIndex_Hook()
 {
 	_asm	mov eax, [esp+4];
@@ -285,19 +209,6 @@ _declspec(naked) void GetLocalPlayerPed_Hook()
 	_asm	retn;
 }
 
-_declspec(naked) void GetPlayerPedFromPlayerInfo_Hook()
-{
-	_asm	mov eax, [esp+4];
-	_asm	mov pReturnedPlayerInfo, eax;
-	_asm	pushad;
-
-	GetPlayerPedFromPlayerInfo(pReturnedPlayerInfo);
-
-	_asm	popad;
-	_asm	mov eax, _pPlayerPed;
-	_asm	retn;
-}
-
 _declspec(naked) void CFunctionRetnPatch()
 {
 	_asm
@@ -312,22 +223,22 @@ _declspec(naked) void CRASH_625F15_HOOK()
 	_asm
 	{
 		test    eax, eax
-			jz              keks
-			cmp     eax, 100000h
-			jl              keks
-			mov     edx, [eax]
+		jz      keks
+		cmp     eax, 100000h
+		jl      keks
+		mov     edx, [eax]
 		push    1
-			mov     ecx, edi
-			call    edx
+		mov     ecx, edi
+		call    edx
 
 keks_patch:
 		mov     al, 1
-			pop     edi
-			pop     esi
-			pop     ebp
-			pop     ebx
-			add     esp, 0Ch
-			retn    4
+		pop     edi
+		pop     esi
+		pop     ebp
+		pop     ebx
+		add     esp, 0Ch
+		retn    4
 keks:
 		pushad
 	}
@@ -337,64 +248,13 @@ keks:
 	_asm
 	{
 		popad
-			jmp keks_patch
+		jmp keks_patch
 	}
 }
-
-_declspec(naked) void CGameProcessHook()
-{
-	if(!bInitialiseGame && iFrameCalls != 300)
-	{
-		iFrameCalls++;
-		_asm	mov ebp, esp;
-		_asm	jmp COffsets::IV_Hook__GameProcess_1;
-	}
-	else
-	{
-		iFrameCalls++;
-		bInitialiseGame = false;
-
-		// Don't process game this time, just start it(works ram usage increases up to ~1,2GB)
-		_asm	mov ebp, esp;
-		_asm	jmp COffsets::IV_Hook__GameProcess_2;
-
-		BYTE b1 = *(BYTE*)COffsets::IV_Hook__GameProcess_3 = 0;
-		BYTE b2 = *(BYTE*)COffsets::IV_Hook__GameProcess_4 = 1;
-		BYTE b3 = *(BYTE*)COffsets::IV_Hook__GameProcess_5 = 0;
-
-		*(DWORD*)(g_pCore->GetBase()+0x10C7854) = 50;
-
-
-		DWORD keks = *(DWORD *)COffsets::IV_Hook__GameProcess_6; // keks copyright by xforce
-		DWORD g_rgsc = *(DWORD *)COffsets::IV_Hook__GameProcess_7;
-		int iTime = timeGetTime();
-
-		_asm	push 0;
-		_asm	push 1;
-		_asm	push 0;
-		_asm	mov eax, keks;
-		_asm	mov ecx, g_rgsc;
-		_asm	mov edi, iTime;
-		_asm	call COffsets::IV_Hook__GameProcess_8;
-		_asm	add esp, 0Ch;
-	}
-}
-
 
 static int mulPoolSize = 1;
-int __stdcall  CPool_hook_chunk(void* this_, int maxObjects, const char* Name, int entrySize);
-__declspec(naked) void __stdcall CPool_hook()
+int __stdcall  CPool_hook_chunk(IVPool *pPool, int maxObjects, const char* Name, int entrySize)
 {
-	_asm	pop eax
-	_asm	push ecx
-	_asm	push eax
-	_asm	jmp CPool_hook_chunk
-}
-
-int __stdcall  CPool_hook_chunk(void* this_, int maxObjects, const char* Name, int entrySize)
-{
-	IVPool *pPool = (IVPool*)this_;
-
 	if(pPool)
 	{
 
@@ -416,7 +276,6 @@ int __stdcall  CPool_hook_chunk(void* this_, int maxObjects, const char* Name, i
 			pPool->m_nTop = -1;
 
 			int n = 0;
-			char flag;
 			int v5 = 0;
 			BYTE* v8;
 			BYTE v7;
@@ -442,7 +301,6 @@ int __stdcall  CPool_hook_chunk(void* this_, int maxObjects, const char* Name, i
 			pPool->m_nTop = -1;
 
 			int n = 0;
-			char flag;
 			int v5 = 0;
 			BYTE* v8;
 			BYTE v7;
@@ -461,53 +319,13 @@ int __stdcall  CPool_hook_chunk(void* this_, int maxObjects, const char* Name, i
 	return 0;
 }
 
-_declspec(naked) void __stdcall SetupPool_Hook()
+__declspec(naked) void __stdcall CPool_hook()
 {
-	IVPool *pPool;
-
-	_asm	mov pPool, esi;
-	_asm	pushad;
-
-	int entrySize;
-	int maxObjects;
-	maxObjects = pPool->m_dwCount;
-	entrySize = pPool->m_dwEntrySize;
-	maxObjects *= mulPoolSize;
-
-	pPool->m_dwEntrySize = entrySize;
-
-	CGameFunction::Free(pPool->m_pObjects);
-	CGameFunction::Free(pPool->m_pFlags);
-
-	pPool->m_pObjects = (BYTE*)CGameFunction::Alloc(entrySize * maxObjects);
-	pPool->m_pFlags = (BYTE*)CGameFunction::Alloc(maxObjects);
-
-	pPool->m_bAllocated = 1;
-	pPool->m_dwCount = maxObjects;
-	pPool->m_nTop = -1;
-
-	int n;
-	n = 0;
-	char flag;
-	int v5;
-	v5 = 0;
-	BYTE* v8;
-	BYTE v7;
-	BYTE* v6;
-
-	for(pPool->m_dwUsed = 0; v5 < maxObjects; *v8 = v7 & 0x81 | 1)
-	{
-		*(pPool->m_pFlags + v5) |= 0x80;
-		v6 = pPool->m_pFlags;
-		v7 = *(v6 + v5);
-		v8 = v5++ +v6;
-	}
-
-	_asm	popad;
-	_asm	mov eax, pPool;
-	_asm	retn;
+	_asm	pop eax
+	_asm	push ecx
+	_asm	push eax
+	_asm	jmp CPool_hook_chunk
 }
-
 
 /*
 This will multiply the size of the given pools by the value in multi [default: 4]
@@ -518,201 +336,8 @@ void CHooks::IncreasePoolSizes(int multi)
 	CPatcher::InstallJmpPatch(COffsets::IV_Hook__IncreasePoolSizes, (DWORD)CPool_hook);
 }
 
-DWORD PhysicsEAXHandle;
-DWORD IV_GTAPhysicsUpdate;
-__declspec(naked) void __stdcall GTAPhysicsUpdate()
-{
-	_asm mov PhysicsEAXHandle, eax;
-	_asm pushad;
-
-	// Check PhysicsUpdate
-	if(PhysicsEAXHandle == NULL || (DWORD *)PhysicsEAXHandle == NULL) {
-		
-		// Failed to get any eax value, exit.
-		_asm popad;
-		_asm mov eax, [ecx+4];
-		_asm push ebx;
-		_asm mov ebx, [eax+14h];
-		_asm mov PhysicsEAXHandle, ebx;
-		_asm pop ebx;
-		_asm pushad;
-
-		if(PhysicsEAXHandle == NULL || (WORD)PhysicsEAXHandle == NULL)
-		{
-			_asm popad;
-			_asm push ebx;
-			_asm push ebp;
-			_asm mov edi, [eax+4];
-			_asm mov eax, [ecx+0Ch];
-			_asm mov edx, eax;
-			_asm mov ebx, esi;
-			_asm mov ecx, ebp
-			_asm add eax, 50h
-			_asm add edx, 40h;
-			_asm add edi, 4;
-			_asm add ebp, 1
-			_asm pop edi;
-			_asm pop esi;
-			_asm pop esp;
-			_asm pop ebx;
-			_asm xor eax, eax;
-			_asm retn 8;
-		}
-		else {
-			IV_GTAPhysicsUpdate = (g_pCore->GetBase() + 0x0063DD40);
-
-			_asm popad;
-			_asm mov eax, [ecx+14h];
-			_asm mov edx, [ecx+8];
-			_asm push eax;
-			_asm push edx;
-			_asm call IV_GTAPhysicsUpdate;
-			_asm retn;
-		}
-	}
-	else {
-		IV_GTAPhysicsUpdate = (g_pCore->GetBase() + 0x0063DD40);
-
-		_asm popad;
-		_asm mov eax, [ecx+14h];
-		_asm mov edx, [ecx+8];
-		_asm push eax;
-		_asm push edx;
-		_asm call IV_GTAPhysicsUpdate;
-		_asm retn;
-	}
-}
-
-int Thread_a1;
-int Thread_a2;
-signed int Thread_ThreadId;
-int Thread_iPriority;
-char *Thread_a5;
-char *Thread_a6;
-signed int GTA_CreateThreadHook(int a1, int a2, signed int ThreadId, int iPriority, const char *a5, char a6)
-{
-	signed int v6; // edi@1
-	LPVOID v7; // esi@5
-	void *v8; // eax@5
-	HANDLE v9; // eax@5
-	void *v10; // edi@5
-	signed int result; // eax@6
-
-	DWORD dwThreadPatchAdd;
-	char * hThreadName;
-	DWORD dwThreadJmpBack;
-	LPVOID lpParameter = *(LPVOID *)(g_pCore->GetBase() + 0x16A9A3C);
-	DWORD dword_16A9774 = (g_pCore->GetBase() + 0x16A9774);
-	DWORD sub_452030 = (g_pCore->GetBase() + 0x452030);
-	void* sub_452350 = *(VOID **)(g_pCore->GetBase() + 0x452350);
-	DWORD dword_16A9A38 = (g_pCore->GetBase() + 0x16A9A38);
-	void* sub_452170 = *(VOID **)(g_pCore->GetBase() + 0x452170);
-
-
-	v6 = ThreadId;
-	if((unsigned int)ThreadId < 0x4000)
-		v6 = 16384;
-
-	_asm push dword_16A9774;
-	_asm call sub_452030;
-
-	if(!dword_16A9774)
-		*(void **)(sub_452350);
-
-	v7 = lpParameter;
-
-	v8 = *(void **)lpParameter;
-	--dword_16A9A38;
-	
-	lpParameter = v8;
-	dword_16A9774 = 0;
-
-	*(DWORD *)v7 = a1;
-	*((DWORD *)v7 + 1) = a2;
-	*((DWORD *)v7 + 2) = *(DWORD *)(*(DWORD *)__readfsdword(44) + 8);
-
-	ThreadId = 0;
-	v9 = CreateThread(0, v6, (LPTHREAD_START_ROUTINE)sub_452170, v7, 4u, (LPDWORD)&ThreadId);
-
-	v10 = v9;
-	
-	if(v9) {
-		SetThreadPriority(v9, iPriority);
-		SetThreadPriorityBoost(v10, 1);
-
-		if(a6)
-			ResumeThread(v10);
-		result = (signed int)v10;
-	}
-	else
-	{
-		*(DWORD *)v7 = (DWORD)lpParameter;
-		++dword_16A9A38;
-		lpParameter = v7;
-		result = -1;
-	}
-	return result;
-}
-
-signed int uiResult;
-_declspec(naked) signed int GTA_CreateThread()
-{
-	// Grab function paramenters
-	_asm push ebp;
-	_asm mov ebp, esp;
-	_asm push esi;
-	_asm push edi;
-	_asm mov eax, [ebp+4];
-	_asm mov Thread_a1, eax;
-	_asm mov eax, [ebp+8];
-	_asm mov Thread_a2, eax;
-	_asm mov eax, [ebp+0Ch];
-	_asm mov Thread_ThreadId, eax;
-	_asm mov eax, [ebp+10h];
-	_asm mov Thread_iPriority, eax;
-	_asm mov eax, [ebp+18h];
-	_asm mov Thread_a5, eax;
-	_asm mov eax, [ebp+14h];
-	_asm mov Thread_a6, eax;
-
-	CLogFile::Printf("GTA_CreateThread: %d, %d, %d, %d, %s, %s",Thread_a1, Thread_a2, Thread_ThreadId, Thread_iPriority, Thread_a5, Thread_a6);
-
-	uiResult = GTA_CreateThreadHook(Thread_a1, Thread_a2, Thread_ThreadId, Thread_iPriority, Thread_a5, *Thread_a6);
-	
-	_asm mov eax, uiResult;
-	_asm pop esi;
-	_asm pop edi;
-	_asm retn;
-}
-void log(const char *a1, ...)
-{
-	CLogFile::Printf("%s", 1);
-}
-
-void GTA_LOG(DWORD a1, int a2, DWORD a3 = NULL, DWORD a4 = NULL, ...)
-{
-	// Print for a3
-	va_list vaArgs;
-	char szBuffer[2048];
-	va_start(vaArgs, a3);
-	vsnprintf_s(szBuffer, sizeof(szBuffer), (char *)a3, vaArgs);
-	va_end(vaArgs);
-
-	// Print for a4
-	vaArgs;
-	szBuffer[2048];
-	va_start(vaArgs, a4);
-	vsnprintf_s(szBuffer, sizeof(szBuffer), (char *)a4, vaArgs);
-	va_end(vaArgs);
-
-	log((const char *)(strlen((char *)a3) != 0 ? a3 : a4));
-}
-
 void CHooks::Intialize()
 {
-	// Hook physics update
-	//CPatcher::InstallJmpPatch((g_pCore->)63DD30), (DWORD)IV_GTAPhysicsUpdate, 3);
-	
 	// Hook CEpisodes::IsEpisodeAvaliable to use our own function
 	//CPatcher::InstallJmpPatch(COffsets::FUNC_CEpisodes__IsEpisodeAvaliable, (DWORD)CEpisodes__IsEpisodeAvaliable_Hook);
 	
@@ -738,7 +363,7 @@ void CHooks::Intialize()
 	CPatcher::InstallNopPatch(COffsets::CALL_StartLoadingTune, 5);
 	
 	// Hook texture select/generate function
-	CPatcher::InstallJmpPatch(COffsets::FUNC_GENERATETEXTURE, (DWORD)TextureSelect_Hook);
+	//CPatcher::InstallJmpPatch(COffsets::FUNC_GENERATETEXTURE, (DWORD)TextureSelect_Hook);
 	
 	// This disables some calculate for modelinfo but it seems this is not necessary
 	CPatcher::InstallJmpPatch(COffsets::IV_Hook__UnkownPatch1, (COffsets::IV_Hook__UnkownPatch1 + 0x40));
